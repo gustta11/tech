90b6d06adf55356e2c99c9da34e848b1fcb3593b
gustta11 <gustavokeven50@gmail.com>
2024-09-02 21:08:46 -0300

    #style: Melhorando a disposição dos produtos

--- src/components/Produtos.jsx
+++ src/components/Produtos.jsx
@@ -12,22 +12,76 @@ const Produtos =() => {
     return(
         <div>
             <section className='produtos'>
-                 
-                  <div className='produto_card'><img src={ssd} alt="" /></div>
-                  <p>jsjsj</p>
-                  <div className='produto_card'><img src={teclado} alt="" /></div>
-                  <p>jsjsj</p>
-                  <div className='produto_card'><img src={Notebook} alt="ss"  className='note'/></div>
-                  <p>jsjsj</p>
 
-             
-         
-                  <div className='produto_card'><img src={memoriaRam} alt="" /></div>
-                  <p>jsjsj</p>
-                  <div className='produto_card'><img src={gabinete} alt="" /></div>
-                  <p>jsjsj</p>
-                  <div className='produto_card'><img src={headset} alt="" /></div>
-                  <p>jsjsj</p>
+                    <div className='card_produto'>
+                        <div className='card_produto_imagem'>
+                            <img src={gabinete}  />
+                        </div>
+                        <div className='card_produto_preco'>
+                            <p className='preco_oferta'>20%off</p>
+                            <p>R$ 2.500 <span>De: R$4.000</span> </p>
+                            <p>Gabinete</p>
+                        </div>
+                    </div>
+
+                    <div className='card_produto'>
+                        <div className='card_produto_imagem'>
+                            <img src={headset}  />
+                        </div>
+                        <div className='card_produto_preco'>
+                            <p className='preco_oferta'>20%off</p>
+                            <p>R$ 2.500 <span>De: R$4.000</span></p>
+                            <p>Headset</p>
+                        </div>
+                    </div>
+
+                    <div className='card_produto'>
+                        <div  id='memoriaRam'  className='card_produto_imagem'>
+                            <img src={memoriaRam}  />
+                        </div>
+                        <div className='card_produto_preco'>
+                            <p className='preco_oferta'>20%off</p>
+                            <p>R$ 2.500 <span>De: R$4.000</span></p>
+                            <p>Memória</p>
+                        </div>
+                    </div>
+
+                    <div className='card_produto'>
+                        <div className='card_produto_imagem'>
+                            <img src={teclado}  />
+                        </div>
+                        <div className='card_produto_preco'>
+                            <p>Teclado</p>
+                            <p>R$ 2.500</p>
+                            <p  className='card_produto_adicionaraocarrinho' >Adicionar ao carrinho</p>
+                            
+                        </div>
+                    </div>
+
+                    <div className='card_produto'>
+                        <div className='card_produto_imagem'>
+                            <img src={ssd}  />
+                        </div>
+                        <div className='card_produto_preco'>
+                            <p>Ssd</p>
+                            <p>R$ 2.500</p>
+                            <p className='card_produto_adicionaraocarrinho' >Adicionar ao carrinho</p>
+                            
+                        </div>
+                    </div>
+
+                    <div className='card_produto'>
+                        <div className='card_produto_imagem'>
+                            <img src={Notebook}  />
+                        </div>
+                        <div className='card_produto_preco'>
+                            <p>Notebook</p>
+                            <p>R$ 2.500</p>
+                            <p className='card_produto_adicionaraocarrinho'>Adicionar ao carrinho</p>
+                           
+                        </div>
+                    </div>
+                  
                  
             
             </section>
